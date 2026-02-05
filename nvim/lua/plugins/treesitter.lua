57a77271d07f44c455e2a09737b546ff52d5f779
Loki Astari <Loki.Astari@gmail.com>
2026-02-05 12:49:31 -0800

    Update to latest

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,42 +4,42 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    -- main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-         ensure_installed = {
+        ensure_installed = {
             'bash',
-            --'c',
+            'c',
             'clojure',
             'css',
-            --'cpp',
-            --'diff',
-            --'dockerfile',
+            'cpp',
+            'diff',
+            'dockerfile',
             'fennel',
-            --'gitignore',
+            'gitignore',
             'html',
-            --'java',
-            --'javascript',
-            --'json',
-            --'lua',
-            --'luadoc',
-            --'make',
+            'java',
+            'javascript',
+            'json',
+            'lua',
+            'luadoc',
+            'make',
             'markdown',
             'markdown_inline',
-            --'python',
-            --'terraform',
-            --'toml',
-            --'typescript',
-            --'query',
-            --'vim',
-            --'vimdoc',
+            'python',
+            'terraform',
+            'toml',
+            'typescript',
+            'query',
+            'vim',
+            'vimdoc',
             'yaml'
-    },
-    auto_install = true,
-    highlight = {
-        enable = true,
-    },
-    indent = { enable = true, disable = { 'ruby', 'cpp' } },
+        },
+        auto_install = true,
+        highlight = {
+            enable = true,
+        },
+        indent = { enable = true, disable = { 'ruby', 'cpp' } },
     },
 }
 
