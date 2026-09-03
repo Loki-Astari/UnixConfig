6b029a0c2fd593f98d9005a1253f2a45f064f130
Loki Astari <Loki.Astari@gmail.com>
2026-09-03 13:45:43 -0700

    Update key bindings with new Agent Features

--- nvim/lua/plugins/AIAgent.lua
+++ nvim/lua/plugins/AIAgent.lua
@@ -15,12 +15,16 @@ return {
            {'<leader>ao', ':AgentOpen<cr>', desc = "Open agent (default)"},
            {'<leader>ac', ':AgentOpen Cursor<cr>', desc = "Open Cursor agent"},
            {'<leader>ax', ':AgentClose<cr>', desc = "Close current agent" },
-           {'<leader>at', ':AgentToggle<cr>', desc = "Toggle current agent" },
+           {'<leader>aT', ':AgentToggle<cr>', desc = "Toggle current agent" },
            {'<leader>as', ':AgentSendSelection<cr>', desc = "Send selection to agent" },
            {'<leader>ad', ':AgentSendDiagnostics<cr>', desc = "Send Diagnostic to agent" },
            {'<leader>a ', ':AgentSessions<cr>', desc = "Open the Agent Session Selector"},
            {'<leader>ar', ':AgentSessions!<cr>', desc = "Replays the Agent Session Selector"},
            {'<leader>ah', ':AgentDiff<cr>', desc = "Open the Agent Diff Viewer"},
+           {'<leader>al', ':AgentList!<cr>', desc = "List agents in all Neovim instances"},
+           {'<leader>ag', ':AgentTree<cr>', desc = "Session history tree"},
+           {'<leader>af', ':AgentFind<cr>', desc = "Find an old Session"},
+           {'<leader>at', ':AgentTree<cr>', desc = "Agent Session Tree"},
         })
         wk.add({
            {'<leader>sc', ':set ic!<CR>',   desc = 'Toggle case sensative search'}
